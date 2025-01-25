90945f9fc729b73c582848b8f6644134e2417ef4
unknown <areyad203011@bscse.uiu.ac.bd>
2025-01-25 21:01:06 +0600

    project done

--- js/script.js
+++ js/script.js
@@ -27,3 +27,51 @@ faq.forEach((question) => {
     }
   });
 });
+
+// I consider only a single image will change, so I do it static way.
+
+const business_next = document.getElementById("business-next");
+const business_prev = document.getElementById("business-prev");
+const brands_next = document.getElementById("brands-next");
+const brands_prev = document.getElementById("brands-prev");
+const business_last_img = document.getElementById("business-last-img");
+const business_first_img = document.getElementById("business-first-img");
+const brands_last_img = document.getElementById("brands-last-img");
+const brands_first_img = document.getElementById("brands-first-img");
+
+business_next.addEventListener("click", () => {
+  business_first_img.style.display = "none";
+  business_last_img.style.display = "flex";
+  business_next.style.display = "none";
+  business_prev.style.display = "block";
+});
+
+business_prev.addEventListener("click", () => {
+  business_last_img.style.display = "none";
+  business_first_img.style.display = "flex";
+  business_next.style.display = "block";
+  business_prev.style.display = "none";
+});
+
+brands_next.addEventListener("click", () => {
+  brands_first_img.style.display = "none";
+  brands_last_img.style.display = "flex";
+  brands_next.style.display = "none";
+  brands_prev.style.display = "block";
+});
+
+brands_prev.addEventListener("click", () => {
+  brands_last_img.style.display = "none";
+  brands_first_img.style.display = "flex";
+  brands_prev.style.display = "none";
+  brands_next.style.display = "block";
+});
+
+function showNavlinks(){
+    const navLinks = document.getElementById("links-for-mobile");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+      } else {
+        navLinks.style.display = "block";
+      }
+}
